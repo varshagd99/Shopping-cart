155de013024aef7b1e104d8bb5852ac0a6ee72e1
varshagd <varshagundoomane@gmail.com>
2020-06-09 07:40:37 +0530

    change to the style

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@
      url:"mongodb+srv://varsha:dattebayo@db-iinqi.mongodb.net/test?retryWrites=true&w=majority"
  }
 
-//module.exports={
-  //  url:'mongodb://localhost:27017/cart'
-//}
+// module.exports={
+//    url:'mongodb://localhost:27017/cart'
+// }
